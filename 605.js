29c2b64b64f62a18b78a07c1f2e84cc402c679cc
Fenics-artem <lesnoi_artem@mail.ru>
2021-08-24 16:46:40 +0700

    1
    2

---
+++ 605.js
@@ -0,0 +1,52 @@
+"use strict";
+(self["webpackChunk_vue3_demo_home"] = self["webpackChunk_vue3_demo_home"] || []).push([[605,406],{
+
+/***/ 605:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Content)
+});
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(252);
+// EXTERNAL MODULE: ./node_modules/@vue/shared/dist/shared.esm-bundler.js
+var shared_esm_bundler = __webpack_require__(577);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Content.vue?vue&type=template&id=54fda9af
+
+
+const _hoisted_1 = { style: {"color":"red"} }
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return ((0,runtime_core_esm_bundler/* openBlock */.wg)(), (0,runtime_core_esm_bundler/* createBlock */.j4)("div", _hoisted_1, (0,shared_esm_bundler/* toDisplayString */.zw)($data.title), 1 /* TEXT */))
+}
+;// CONCATENATED MODULE: ./src/components/Content.vue?vue&type=template&id=54fda9af
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Content.vue?vue&type=script&lang=js
+
+/* harmony default export */ const Contentvue_type_script_lang_js = ({
+  data() {
+    return {
+      title: "Remote Component in Action..",
+    };
+  },
+});
+
+;// CONCATENATED MODULE: ./src/components/Content.vue?vue&type=script&lang=js
+ 
+;// CONCATENATED MODULE: ./src/components/Content.vue
+
+
+
+Contentvue_type_script_lang_js.render = render
+
+/* harmony default export */ const Content = (Contentvue_type_script_lang_js);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=605.js.map
