61991667c1ba70d646cfa7d1ba0a8b80cb605104
Lesnoy Artem <a.lesnoy@n1.ru>
2021-09-05 16:50:18 +0700

    123

---
+++ 2.0/488.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunk_vue3_demo_home"] = self["webpackChunk_vue3_demo_home"] || []).push([[488,347],{
+
+/***/ 347:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(252);
+
+const button = {
+  name: "btn-component",
+  render() {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.h)(
+      "button",
+      {
+        id: "btn-primary",
+      },
+      "Hello World"
+    );
+  },
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (button);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=488.js.map
---
+++ 2.0/99.js
@@ -0,0 +1,60 @@
+"use strict";
+(self["webpackChunk_vue3_demo_home"] = self["webpackChunk_vue3_demo_home"] || []).push([[99,580],{
+
+/***/ 580:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Content)
+});
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(252);
+// EXTERNAL MODULE: ./node_modules/@vue/shared/dist/shared.esm-bundler.js
+var shared_esm_bundler = __webpack_require__(577);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Content.vue?vue&type=template&id=2b5ce78a
+
+
+const _hoisted_1 = {
+  class: "rrr",
+  style: {"color":"red"}
+}
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return ((0,runtime_core_esm_bundler/* openBlock */.wg)(), (0,runtime_core_esm_bundler/* createBlock */.j4)("div", _hoisted_1, (0,shared_esm_bundler/* toDisplayString */.zw)($data.title), 1 /* TEXT */))
+}
+;// CONCATENATED MODULE: ./src/components/Content.vue?vue&type=template&id=2b5ce78a
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Content.vue?vue&type=script&lang=js
+
+/* harmony default export */ const Contentvue_type_script_lang_js = ({
+  data() {
+    return {
+      title: "Remote Component in Action222..",
+    };
+  },
+    mounted() {
+        this.$el.parentElement.parentElement.setAttribute('eeeeeeeeeeeeee', 123321)
+    }
+});
+
+;// CONCATENATED MODULE: ./src/components/Content.vue?vue&type=script&lang=js
+ 
+;// CONCATENATED MODULE: ./src/components/Content.vue
+
+
+
+
+;
+Contentvue_type_script_lang_js.render = render
+
+/* harmony default export */ const Content = (Contentvue_type_script_lang_js);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=99.js.map
